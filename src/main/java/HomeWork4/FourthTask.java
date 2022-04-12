932b39a814db3d0eac4a3c8826762ffe4c3bc1db
User <a>
2022-04-12 15:50:54 +0300

    Fourth Task

---
+++ src/main/java/HomeWork4/FourthTask.java
@@ -0,0 +1,19 @@
+package HomeWork4;
+
+public class FourthTask {
+    public static void main(String[] args) {
+        printColor();
+    }
+    public static void printColor(){
+        int value = -101;
+
+        if (value <=0){
+            System.out.println("Красный");
+        }else if(value <= 100){
+            System.out.println("Желтый");
+        }else {
+            System.out.println("Зеленый");
+        }
+    }
+
+}
