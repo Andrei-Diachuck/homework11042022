a2684f05ba84f4da014c7a53a0ae79cd1fc6e2de
User <a>
2022-04-12 14:43:43 +0300

    Second first

---
+++ src/main/java/HomeWork2/SecondFirst.java
@@ -0,0 +1,14 @@
+package HomeWork2;
+
+public class SecondFirst {
+
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+}
