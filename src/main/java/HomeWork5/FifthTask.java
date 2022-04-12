50444ae5616c497453cefc2561c32564ab51283f
User <a>
2022-04-12 15:55:07 +0300

    Fifth Task

---
+++ src/main/java/HomeWork5/FifthTask.java
@@ -0,0 +1,18 @@
+package HomeWork5;
+
+public class FifthTask {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void compareNumbers(){
+        int a = 8;
+        int b = 8;
+
+        if (a >= b){
+            System.out.println("a >= b");
+        }else {
+            System.out.println("a < b");
+        }
+    }
+}
