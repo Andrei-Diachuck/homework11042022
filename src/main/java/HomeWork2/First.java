4a311d21df03ed9e0628a8f79e7f4f1b809a753b
User <a>
2022-04-15 00:31:36 +0300

    first

--- src/main/java/HomeWork2/First.java
+++ src/main/java/HomeWork2/First.java
@@ -1,4 +1,16 @@
 package HomeWork2;
 
 public class First {
+    public static void main(String[] args) {
+        System.out.println(examination(5, 20));
+    }
+
+    public static boolean examination(int a, int b) {
+        int sum = a + b;
+        while (sum > 10 && sum <= 20) {
+            return true;
+        }
+        return false;
+    }
+
 }
